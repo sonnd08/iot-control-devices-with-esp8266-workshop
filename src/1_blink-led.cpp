039d34f5f4f8f8cf59d1f6e46fa3c1426d4cd669
Nguyễn Đình Sơn <sonnd08@gmail.com>
2023-10-30 12:10:48 +0700

    switch to D1 pin

--- src/1_blink-led.cpp
+++ src/1_blink-led.cpp
@@ -5,18 +5,18 @@ void setup() {
 
   // Initialize GPIO15 and GPIO2 as an output
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(D8, OUTPUT);
+  pinMode(D1, OUTPUT);
 }
 
 void loop() {
   // Turn the LED on
-  digitalWrite(LED_BUILTIN, HIGH); // in nodeMcu, LED buildin Low=HIGH = off
-  digitalWrite(D8, HIGH);
+  digitalWrite(LED_BUILTIN, LOW); // in nodeMcu, LED buildin Low=HIGH = off
+  digitalWrite(D1, HIGH);
   delay(500); // Wait for 500 milliseconds (0.5 seconds)
 
   // Turn the LED off
   digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(D8, LOW);
+  digitalWrite(D1, LOW);
   // digitalWrite(8, LOW);
   delay(500); // Wait for 500 milliseconds (0.5 seconds)
 }
